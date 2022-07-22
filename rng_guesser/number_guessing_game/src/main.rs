1fcd893ae8926b17c1c591082b76433671b893a3
Lucas Nguyen <lucasoAlt@outlook.com>
2022-07-22 13:46:43 +0200

    Create code for RNG guesser

---
+++ rng_guesser/number_guessing_game/src/main.rs
@@ -0,0 +1,60 @@
+use rand::thread_rng;
+use rand::Rng;
+
+use std::io;
+
+fn main() {
+    println!("Program Started...");
+
+    let start_txt = "
+██████╗     ███╗   ██╗     ██████╗              ██████╗ ██╗   ██╗███████╗███████╗███████╗███████╗██████╗ 
+██╔══██╗    ████╗  ██║    ██╔════╝             ██╔════╝ ██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔══██╗
+██████╔╝    ██╔██╗ ██║    ██║  ███╗            ██║  ███╗██║   ██║█████╗  ███████╗███████╗█████╗  ██████╔╝
+██╔══██╗    ██║╚██╗██║    ██║   ██║            ██║   ██║██║   ██║██╔══╝  ╚════██║╚════██║██╔══╝  ██╔══██╗
+██║  ██║    ██║ ╚████║    ╚██████╔╝            ╚██████╔╝╚██████╔╝███████╗███████║███████║███████╗██║  ██║
+╚═╝  ╚═╝    ╚═╝  ╚═══╝     ╚═════╝              ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝                                                                                                                                                                                                                                         
+    ";
+
+    println!("{}", start_txt);
+    println!("Made by lucasodev - UNLICENSE");
+    println!(" ");
+    println!(" ");
+ 
+
+    guessing();
+}
+
+fn guessing() {
+    let random_number = get_random_number();
+    
+    let mut guess_right: bool = false;
+
+    while guess_right == false {
+        let guess = get_user_input();
+        if guess == random_number {
+            println!("Yeah, it was right, the number was {}!", random_number);
+            guess_right = true;
+        } else {
+            println!("Sorry, Your answer was wrong!");
+            continue;
+        }
+    }
+}
+
+fn get_random_number() -> i64 {
+    let _random_number: i64 = thread_rng().gen_range(0, 11);
+
+    return _random_number;
+}
+
+fn get_user_input() -> i64 {
+    println!("Please input a guessing number under (Must be between 0 - 10):");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error while reading line!");
+
+    let _int_guess: i64 = guess.trim().parse().unwrap();
+
+    return _int_guess;
+}
